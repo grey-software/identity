3c4fac4c201f610cb438ce9f405bc48625ffac0c
Arsala Grey <arsala@grey.software>
2023-06-04 21:00:55 +0500

    trying esm.sh import

--- netlify/edge-functions/auth.ts
+++ netlify/edge-functions/auth.ts
@@ -1,22 +1,23 @@
-import * as netlify from "netlify-auth-providers"
+import netlify from "https://esm.sh/netlify-auth-providers";
 
 export default () => {
-    const authenticator = new netlify.default({});
-    authenticator.authenticate(
-        // Set the OAuth provider and token scope
-        // Provider can be "github", "gitlab", or "bitbucket"
-        // The scopes available depend on your OAuth provider
-        { provider: "github", scope: "user" },
-        async function (error: any, data: any) {
-            if (error) {
-                return new Response("Error Authenticating with GitHub: " + error)
-            } else {
-                let responseString = "Authenticated with GitHub. Access Token: " + data.token
-                responseString += "\n"
-                return new Response(responseString)
-            }
-        }
-    );
+  const authenticator = new netlify.default({});
+  authenticator.authenticate(
+    // Set the OAuth provider and token scope
+    // Provider can be "github", "gitlab", or "bitbucket"
+    // The scopes available depend on your OAuth provider
+    { provider: "github", scope: "user" },
+    async function (error: any, data: any) {
+      if (error) {
+        return new Response("Error Authenticating with GitHub: " + error);
+      } else {
+        let responseString =
+          "Authenticated with GitHub. Access Token: " + data.token;
+        responseString += "\n";
+        return new Response(responseString);
+      }
+    }
+  );
 };
 
 export const config = { path: "/auth" };
